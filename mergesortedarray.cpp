19a085ac50f75f7863ecda0a9788f97300dcf634
Siddarda Azmeera <90519635+SiddardaAzmeera@users.noreply.github.com>
2023-08-10 20:47:12 +0530

    Update mergesortedarray.cpp

--- mergesortedarray.cpp
+++ mergesortedarray.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    void merge(vector<int>&nums1,int m, vector<int>&nums2,int n){
-        for(int j=0,i=m;j<n;j++){
-            nums1[i]=nums2[j];
-            i++;
-
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j =n-1;
+        int k = m+n-1;
+        while(i >=0 && j >=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }else {
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
         }
-        sort(nums1.begin(),nums1.end());
+            while(j>=0){
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            
+        }
     }
 };
 /* Merge sort is an efficient, general-purpose, comparison-based sorting algorithm. 
